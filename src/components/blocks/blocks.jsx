2ff5cb332dd2336fd68fdb3e5a33832189635650
Kaue <kaue.drigo@cybergenios.com.br>
2023-04-20 10:28:16 -0300

    feat: add an element to work as shadow for blocklySvg and toolbox-flyout

--- src/components/blocks/blocks.jsx
+++ src/components/blocks/blocks.jsx
@@ -11,13 +11,43 @@ const BlocksComponent = props => {
         ...componentProps
     } = props;
     return (
-        <Box
-            className={classNames(styles.blocks, {
-                [styles.dragOver]: dragOver
-            })}
-            {...componentProps}
-            componentRef={containerRef}
-        />
+        <>
+            <Box
+                className={classNames(styles.blocks, {
+                    [styles.dragOver]: dragOver
+                })}
+                {...componentProps}
+                componentRef={containerRef}
+            />
+
+            <div
+                style={{
+                    backgroundColor: 'transparent',
+                    width: 320,
+                    height: '100%',
+                    position: 'absolute',
+                    top: 0,
+                    left: 110,
+                    borderRadius: 16,
+                    boxShadow: '2px 5px 7px rgba(0, 0, 0, 0.1)',
+                    pointerEvents: 'none'
+                }}
+            />
+
+            <div
+                style={{
+                    backgroundColor: 'transparent',
+                    width: 'calc(100% - 446px)',
+                    height: '100%',
+                    position: 'absolute',
+                    top: 0,
+                    right: 0,
+                    borderRadius: 16,
+                    boxShadow: '2px 5px 7px rgba(0, 0, 0, 0.1)',
+                    pointerEvents: 'none'
+                }}
+            />
+        </>
     );
 };
 BlocksComponent.propTypes = {
